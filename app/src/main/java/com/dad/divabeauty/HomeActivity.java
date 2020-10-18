f6f8838c99cf6ac5a17407b9b450420bf9942cc7
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-10-18 22:20:13 +0700

    main, login, register

--- app/src/main/java/com/dad/divabeauty/HomeActivity.java
+++ app/src/main/java/com/dad/divabeauty/HomeActivity.java
@@ -1,14 +1,29 @@
 package com.dad.divabeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        recyclerView = (RecyclerView) findViewById(R.id.recyler_view_dokter);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 }
