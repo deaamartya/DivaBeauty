45820e5b43c250b7300a145881321a4c8a2579fb
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-10-18 20:12:30 +0700

    main, login, register

--- app/src/main/java/com/dad/divabeauty/RegisterActivity.java
+++ app/src/main/java/com/dad/divabeauty/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.dad.divabeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button register = findViewById(R.id.register_btn_create);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button login = findViewById(R.id.register_btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
