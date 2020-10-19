30bcd2ad77f2aa19d926eaeb54bf98352bdb7ef9
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-10-19 16:16:35 +0700

    main, login, register

---
+++ app/src/main/java/com/dad/divabeauty/RiwayatKonsultasi.java
@@ -0,0 +1,14 @@
+package com.dad.divabeauty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RiwayatKonsultasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riwayat_konsultasi);
+    }
+}
