0c98b92826253241a3ea0e9e8a10dac432aaece3
deaamartya <50395247+deaamartya@users.noreply.github.com>
2020-10-19 17:23:58 +0700

    main, login, register

--- app/src/main/java/com/dad/divabeauty/HomeActivity.java
+++ app/src/main/java/com/dad/divabeauty/HomeActivity.java
@@ -1,15 +1,20 @@
 package com.dad.divabeauty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private CardView card_pemeriksaan;
+    private CardView card_riwayat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +30,21 @@ public class HomeActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
+        card_pemeriksaan = findViewById(R.id.card_pemeriksaan);
+        card_pemeriksaan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        card_riwayat = findViewById(R.id.card_riwayat);
+        card_riwayat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, RiwayatKonsultasi.class);
+                startActivity(intent);
+            }
+        });
     }
 }
